19df3db408532fe54bfa4f14752eec02584c5f22
Ahsen Khaliq <ahsen.khaliq@gmail.com>
2021-08-16 17:15:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def run_cmd(command):
     except KeyboardInterrupt:
         print("Process interrupted")
         sys.exit(1)
-run_cmd("wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P experiments/pretrained_models")
+run_cmd("wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P ./experiments/pretrained_models")
 run_cmd("python setup.py develop")
 
 
